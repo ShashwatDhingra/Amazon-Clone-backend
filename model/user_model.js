39ffa0e6b0d2b950fbfda5eeade7caf7c87022db
Shashwat <shashwatdhingra2@gmail.com>
2024-01-04 22:30:09 +0530

    apply validation on email in node model

--- model/user_model.js
+++ model/user_model.js
@@ -4,7 +4,14 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true },
-    email: { type: String, required: true, unique: true },
+    email: {
+        type: String, required: true, unique: true, validate: {
+            validator: function (value) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+            },
+            message: 'Invalid email format',
+        },
+    },
     password: { type: String, required: true },
     address: { type: String, default: '' },
     type: {
