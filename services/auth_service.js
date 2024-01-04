10c282fe81ddba63c41f96a3022632d0af591097
Shashwat <shashwatdhingra2@gmail.com>
2024-01-04 22:41:26 +0530

    refactor

--- services/auth_service.js
+++ services/auth_service.js
@@ -7,14 +7,14 @@ class AuthService {
             // Check if the User exists
             const existingUser = await userModel.findOne({ email });
             if (existingUser) {
-                return { status: false, statusCode: 200, message: "Email already exists" }
+                return { status: false, statusCode: 400, message: "Email already exists" }
             }
 
             let newUser = new userModel({ name, email, password });
 
             const user = await newUser.save();
 
-            return { status: true,  statusCode: 400, message: "User created" , user};
+            return { status: true,  statusCode: 200, message: "User created" , user};
         } catch (e) {
             return {status: false, statusCode: 500, error: e.message}
         }
