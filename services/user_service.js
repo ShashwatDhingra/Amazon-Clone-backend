2789fb09d552e0f05f2762d0f672d60ca82b28e3
Shashwat <shashwatdhingra2@gmail.com>
2024-01-14 10:06:55 +0530

    debug

--- services/user_service.js
+++ services/user_service.js
@@ -9,29 +9,28 @@ class UserService {
             console.log(product);
             let user = await userModel.findById(userId);
             
-            // if(user.cart.length == 0){
-            //     user.cart.push({
-            //         product,
-            //         quantity : 1
-            //     })
-            // }else{
-            //     let isProductFound = false;
-            //     for(let i = 0; i < user.cart.length; i++){
-            //         if(user.cart[i].product._id.toString() == product._id){
-            //             isProductFound = true;
-            //             user.cart[i].quantity++;
-            //         }
+            if(user.cart.length == 0){
+                user.cart.push({
+                    product,
+                    quantity : 1
+                })
+            }else{
+                let isProductFound = false;
+                for(let i = 0; i < user.cart.length; i++){
+                    if(user.cart[i].product._id.toString() == product._id){
+                        isProductFound = true;
+                        user.cart[i].quantity++;
+                    }
 
-            //         if(!isProductFound){
-            //             user.cart.push({
-            //                 product,
-            //                 quantity : 1
-            //             })
-            //         }
-            //     }
-            // }
+                    if(!isProductFound){
+                        user.cart.push({
+                            product,
+                            quantity : 1
+                        })
+                    }
+                }
+            }
 
-            user.cart = [];
             user = await user.save();
 
             return { status: true, statusCode: 200, user};
